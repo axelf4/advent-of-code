34b4f27ee7381ba6f31831b446959cd7984adc78
Axel Forsman <axelsfor@gmail.com>
2019-12-14 22:14:07 +0100

    Add first part of day 12

---
+++ nbody.hs
@@ -0,0 +1,68 @@
+data Vector a =
+  Vector a a a
+  deriving (Show)
+
+type IVec = Vector Int
+
+zero = Vector 0 0 0
+
+vmap f (Vector a b c) = Vector (f a) (f b) (f c)
+
+instance Foldable Vector where
+  foldMap f (Vector a b c) = f a `mappend` f b `mappend` f c
+
+instance (Num a) => Num (Vector a) where
+  Vector a b c + Vector x y z = Vector (a + x) (b + y) (c + z)
+  Vector a b c * Vector x y z = Vector (a * x) (b * y) (c * z)
+  negate (Vector a b c) = Vector (-a) (-b) (-c)
+  abs (Vector a b c) = Vector (abs a) (abs b) (abs c)
+  signum (Vector a b c) = Vector (signum a) (signum b) (signum c)
+  fromInteger i = Vector (fromInteger i) (fromInteger i) (fromInteger i)
+
+data Body =
+  Body
+    { pos :: IVec
+    , vel :: IVec
+    }
+  deriving (Show)
+
+applyGravity :: [Body] -> [Body]
+applyGravity bodies =
+  map
+    (\Body {pos = p, vel = v} ->
+       let nv = foldl (\acc x -> acc + signum (pos x - p)) v bodies
+        in Body {pos = p, vel = nv})
+    bodies
+
+applyVelocity :: [Body] -> [Body]
+applyVelocity = map (\Body {pos = p, vel = v} -> Body {pos = p + v, vel = v})
+
+step :: [Body] -> [Body]
+step = applyVelocity . applyGravity
+
+energy :: [Body] -> Int
+energy =
+  sum .
+  map
+    (\Body {pos = p, vel = v} ->
+       let potential = sum (abs p)
+           kinetic = sum (abs v)
+        in potential * kinetic)
+
+printBodies :: [Body] -> IO [()]
+printBodies = sequence . (map (putStrLn . show))
+
+main = do
+  let bodies =
+        [ Body {pos = Vector (-7) (-8) 9, vel = zero}
+        , Body {pos = Vector (-12) (-3) (-4), vel = zero}
+        , Body {pos = Vector 6 (-17) (-9), vel = zero}
+        , Body {pos = Vector 4 (-10) (-6), vel = zero}
+        ]
+  putStrLn "After 0 steps:"
+  printBodies bodies
+  let newBodies = (iterate step bodies) !! 1000
+  putStrLn "After 100 steps:"
+  printBodies newBodies
+  let e = energy newBodies
+  putStrLn ("fin: " ++ (show e))
